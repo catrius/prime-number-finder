fb76630096d0a49a93f856a6af8cc7be1a9a5fb3
Catrius Pham <catriuspham@gmail.com>
2020-09-21 03:44:00 +0700

    [f] Add Calculate button and clear button

--- src/components/homepage/index.jsx
+++ src/components/homepage/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import TextField, { Input } from '@material/react-text-field';
 import { Headline4, Headline5 } from '@material/react-typography';
+import MaterialIcon from '@material/react-material-icon';
+import Button from '@material/react-button';
 
 import styles from './homepage.module.sass';
 
@@ -16,16 +18,27 @@ export default class Homepage extends Component {
       output: 'Input your number above',
     };
 
+    this.calculate = this.calculate.bind(this);
     this.handleInputKeyPress = this.handleInputKeyPress.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.clearInput = this.clearInput.bind(this);
+  }
+
+  calculate() {
+    this.setState({
+      output: calculatePreviousPrimeNumber(this.state.input),
+    });
   }
 
   handleInputKeyPress(e) {
     if (e.key === 'Enter') {
-      this.setState({
-        output: calculatePreviousPrimeNumber(this.state.input),
-      });
+      this.calculate();
     }
+  }
+
+  handleButtonClick() {
+    this.calculate();
   }
 
   handleInputChange(e) {
@@ -34,17 +47,37 @@ export default class Homepage extends Component {
     });
   }
 
+  clearInput() {
+    this.setState({
+      input: '',
+      output: 'Input your number above',
+    });
+  }
+
   render() {
     return (
       <div className={ styles['homepage'] }>
         <Headline4>Previous prime number calculator</Headline4>
-        <TextField className={ styles['input'] }>
+        <TextField
+          className={ styles['input'] }
+          onTrailingIconSelect={ this.clearInput }
+          trailingIcon={ <MaterialIcon role='button' icon='highlight_off' /> }
+        >
           <Input
             value={ this.state.input }
             onKeyPress={ this.handleInputKeyPress }
             onChange={ this.handleInputChange }
+            autoFocus={ true }
           />
         </TextField>
+        <Button
+          className={ styles['calculate-button'] }
+          raised={ true }
+          unelevated={ true }
+          onClick={ this.handleButtonClick }
+        >
+          Calculate
+        </Button>
         <Headline5>{ this.state.output || 'Not found' }</Headline5>
       </div>
     );
--- src/components/tests/homepage.test.js
+++ src/components/tests/homepage.test.js
@@ -1,42 +1,52 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Headline4, Headline5 } from '@material/react-typography';
 import TextField, { Input } from '@material/react-text-field';
+import Button from '@material/react-button';
 
 import Homepage from 'components/homepage';
 
 
 describe('Homepage', function () {
-  it('should render correctly', function () {
+  it('should render correctly the on first time visit', function () {
     const wrapper = shallow(<Homepage />);
-    expect(wrapper.find(Headline4)).toExist();
+    expect(wrapper.find(Headline4).dive()).toHaveText('Previous prime number calculator');
     expect(wrapper.find(TextField)).toExist();
     expect(wrapper.find(TextField).find(Input)).toExist();
-    expect(wrapper.find(Headline5)).toExist();
+    expect(wrapper.find(Headline5).dive()).toHaveText('Input your number above');
   });
 
-  it('should calculate previous prime number', function () {
+  it('should calculate previous prime number if pressing enter when focusing on input', function () {
     const wrapper = shallow(<Homepage />);
     const input = wrapper.find(TextField).find(Input);
 
-    let output = wrapper.find(Headline5);
-    expect(output.dive()).toHaveText('Input your number above');
+    expect(wrapper.find(Headline5).dive()).toHaveText('Input your number above');
 
     input.simulate('change', { currentTarget: { value: 100 } });
     input.simulate('keypress', { key: 'Shift' });
-
-    output = wrapper.find(Headline5);
-    expect(output.dive()).toHaveText('Input your number above');
+    expect(wrapper.find(Headline5).dive()).toHaveText('Input your number above');
 
     input.simulate('keypress', { key: 'Enter' });
-
-    output = wrapper.find(Headline5);
-    expect(output.dive()).toHaveText('97');
+    expect(wrapper.find(Headline5).dive()).toHaveText('97');
 
     input.simulate('change', { currentTarget: { value: 0 } });
     input.simulate('keypress', { key: 'Enter' });
+    expect(wrapper.find(Headline5).dive()).toHaveText('Not found');
+  });
 
-    output = wrapper.find(Headline5);
-    expect(output.dive()).toHaveText('Not found');
+  it('should calculate previous prime number if clicking on calculate button', function () {
+    const wrapper = shallow(<Homepage />);
+    const input = wrapper.find(TextField).find(Input);
+    const button = wrapper.find(Button);
+
+    expect(wrapper.find(Headline5).dive()).toHaveText('Input your number above');
+
+    input.simulate('change', { currentTarget: { value: 100 } });
+    button.simulate('click');
+    expect(wrapper.find(Headline5).dive()).toHaveText('97');
+
+    input.simulate('change', { currentTarget: { value: 0 } });
+    button.simulate('click');
+    expect(wrapper.find(Headline5).dive()).toHaveText('Not found');
   });
 });
